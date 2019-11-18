c98e18bf963a57d4943e8edf68b3cee7f18c4573
joe.wright <joe@joejag.com>
2019-11-18 22:42:18 +0000

    Cors

--- handler.js
+++ handler.js
@@ -10,6 +10,10 @@ export const playlistInfo = (event, context, callback) => {
     if (err) {
       callback(null, {
         statusCode: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
         body: JSON.stringify({
           error: err,
           message: res
@@ -18,6 +22,10 @@ export const playlistInfo = (event, context, callback) => {
     } else {
       callback(null, {
         statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
         body: JSON.stringify(
           res
         )
